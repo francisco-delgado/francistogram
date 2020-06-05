e5d66204841292ea58f3c75edbda7410ac1c6530
Francisco Delgado <francisco@flexport.com>
2020-06-05 15:01:04 -0700

    comment out mailchimp

--- src/components/base/MailchimpEmail.jsx
+++ src/components/base/MailchimpEmail.jsx
@@ -1,28 +1,28 @@
-import * as React from "react";
-import * as EmailValidator from "email-validator";
-import addToMailchimp from "gatsby-plugin-mailchimp";
+// import * as React from "react";
+// import * as EmailValidator from "email-validator";
+// import addToMailchimp from "gatsby-plugin-mailchimp";
 
-export default function MailchimpEmail() {
-  const [email, setEmail] = React.useState(null);
-  const [error, setError] = React.useState(null);
-  const [submitResponse, setSubmitResponse] = React.useState(null);
+// export default function MailchimpEmail() {
+//   const [email, setEmail] = React.useState(null);
+//   const [error, setError] = React.useState(null);
+//   const [submitResponse, setSubmitResponse] = React.useState(null);
 
-  const handleSubmitEmail = () => {
-    const isEmailValid = EmailValidator.validate(email);
-    if (isEmailValid) {
-      submitToMailchimp(email);
-    }
-  };
+//   const handleSubmitEmail = () => {
+//     const isEmailValid = EmailValidator.validate(email);
+//     if (isEmailValid) {
+//       submitToMailchimp(email);
+//     }
+//   };
 
-  const submitToMailchimp = email => {
-    const path = (url =
-      typeof window !== "undefined" ? window.location.href : "");
-    setSubmitResponse(addToMailchimp(email), {
-      path,
-    });
-  };
+//   const submitToMailchimp = email => {
+//     const path = (url =
+//       typeof window !== "undefined" ? window.location.href : "");
+//     setSubmitResponse(addToMailchimp(email), {
+//       path,
+//     });
+//   };
 
-  return (
-    <input onSubmit={this.}
-  )
-}
+//   return (
+//     <input onSubmit={this.}
+//   )
+// }
